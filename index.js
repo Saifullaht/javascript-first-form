c6fe76968b0b595b5ed9fb54ec6816302e0ba1bd
Saifullaht <mrsaifullah025@gmail.in>
2024-03-05 22:51:26 +0500

    this is my first javascript form fullname

---
+++ index.js
@@ -0,0 +1,4 @@
+var firstname = prompt ("Enter your First Name?");
+var lastname = prompt ("Enter your last Name?");
+var fullname = firstname + " " + lastname;
+alert ("Your fullname is " + fullname);
